66393e4ba2a017dd7dd54dcd87f9ac6418a7a677
ixall98 <sunart15up@gmail.com>
2025-05-04 23:16:59 +0700

    Update spam_jadwal.py

--- AyiinXd/modules/spam_jadwal.py
+++ AyiinXd/modules/spam_jadwal.py
@@ -1,5 +1,6 @@
 from AyiinXd import CMD_HANDLER as cmd
 from AyiinXd.ayiin import ayiin_cmd
+from AyiinXd import CMD_HELP
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
