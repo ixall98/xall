13ca44b171722545afd148f381544c5b3a816e31
ixall98 <sunart15up@gmail.com>
2025-05-04 12:14:31 +0700

    Update modul.py

--- AyiinXd/modules/modul.py
+++ AyiinXd/modules/modul.py
@@ -4,6 +4,7 @@ from telethon import Button, events
 from AyiinXd import CMD_HANDLER as cmd
 from AyiinXd import CMD_HELP, tgbot
 from AyiinXd.ayiin import ayiin_cmd
+from AyiinXd import CMD_HELP, tgbot, SUDO_USERS, owner, uid
 
 if tgbot:
 
@@ -31,36 +32,30 @@ if tgbot:
         buttons = paginate_help(0, CMD_HELP, cmd)
         await event.client.send_message(event.chat_id, "• **Daftar Modul:**", buttons=buttons)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ub_modul_(.*)")))
+
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ub_modul_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
         modul_name = event.data_match.group(1).decode("UTF-8")
 
-        cmdhel = str(CMD_HELP[modul_name])
+        cmdhel = str(CMD_HELP.get(modul_name, "Tidak ditemukan."))
         if len(cmdhel) > 950:
             help_string = (
-                str(CMD_HELP[modul_name])
-                .replace("`", "")
-                .replace("**", "")[:950]
+                cmdhel.replace("`", "").replace("**", "")[:950]
                 + "..."
                 + "\n\nBaca Teks Berikutnya Ketik .help "
                 + modul_name
-                + " "
             )
         else:
-            help_string = str(CMD_HELP[modul_name]).replace("`", "").replace("**", "")
+            help_string = cmdhel.replace("`", "").replace("**", "")
 
-        reply_pop_up_alert = (
-            help_string
-            if help_string is not None
-            else "{} Tidak ada dokumen yang telah ditulis untuk modul.".format(
-                modul_name
-            )
-        )
         await event.edit(
-            reply_pop_up_alert,
-            buttons=[Button.inline("ʙᴀᴄᴋ", data="reopen")],
+            help_string,
+            buttons=[Button.inline("ʙᴀᴄᴋ", data=b"reopen")],
         )
     else:
-        reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
-        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        await event.answer(
+            f"Kamu Tidak diizinkan, ini Userbot Milik {owner}",
+            cache_time=0,
+            alert=True,
+        )
