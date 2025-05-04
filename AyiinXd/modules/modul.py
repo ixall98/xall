9a2579cbfad0ac088c1ac072d6175f9a1acb3c64
ixall98 <sunart15up@gmail.com>
2025-05-04 11:57:46 +0700

    Update modul.py

--- AyiinXd/modules/modul.py
+++ AyiinXd/modules/modul.py
@@ -32,37 +32,35 @@ if tgbot:
         await event.client.send_message(event.chat_id, "• **Daftar Modul:**", buttons=buttons)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ub_modul_(.*)")))
-        async def on_plug_in_callback_query_handler(event):
-            if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
-                modul_name = event.data_match.group(1).decode("UTF-8")
+async def on_plug_in_callback_query_handler(event):
+    if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
+        modul_name = event.data_match.group(1).decode("UTF-8")
 
-                cmdhel = str(CMD_HELP[modul_name])
-                if len(cmdhel) > 950:
-                    help_string = (
-                        str(CMD_HELP[modul_name])
-                        .replace("`", "")
-                        .replace("**", "")[:950]
-                        + "..."
-                        + "\n\nBaca Teks Berikutnya Ketik .help "
-                        + modul_name
-                        + " "
-                    )
-                else:
-                    help_string = (str(CMD_HELP[modul_name]).replace(
-                        "`", "").replace("**", ""))
+        cmdhel = str(CMD_HELP[modul_name])
+        if len(cmdhel) > 950:
+            help_string = (
+                str(CMD_HELP[modul_name])
+                .replace("`", "")
+                .replace("**", "")[:950]
+                + "..."
+                + "\n\nBaca Teks Berikutnya Ketik .help "
+                + modul_name
+                + " "
+            )
+        else:
+            help_string = str(CMD_HELP[modul_name]).replace("`", "").replace("**", "")
 
-                reply_pop_up_alert = (
-                    help_string
-                    if help_string is not None
-                    else "{} Tidak ada dokumen yang telah ditulis untuk modul.".format(
-                        modul_name
-                    )
-                )
-                await event.edit(
-                    reply_pop_up_alert, buttons=[
-                        Button.inline("ʙᴀᴄᴋ", data="reopen")]
-                )
-
-            else:
-                reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        reply_pop_up_alert = (
+            help_string
+            if help_string is not None
+            else "{} Tidak ada dokumen yang telah ditulis untuk modul.".format(
+                modul_name
+            )
+        )
+        await event.edit(
+            reply_pop_up_alert,
+            buttons=[Button.inline("ʙᴀᴄᴋ", data="reopen")],
+        )
+    else:
+        reply_pop_up_alert = f"Kamu Tidak diizinkan, ini Userbot Milik {owner}"
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
