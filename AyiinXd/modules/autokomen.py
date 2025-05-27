62367716ccf5d3e7903ad5d95a8195a3df4a8c4f
ixall98 <sunart15up@gmail.com>
2025-05-27 18:29:49 +0700

    Update autokomen.py

--- AyiinXd/modules/autokomen.py
+++ AyiinXd/modules/autokomen.py
@@ -1,6 +1,7 @@
 from telethon import events
-from AyiinXd import client
-from AyiinXd.events import ayiin_cmd
+from AyiinXd import CMD_HANDLER as cmd
+from AyiinXd import CMD_HELP
+from AyiinXd import ayiin_cmd
 
 # Struktur data: 
 # {channel_id: {filter_text: response_text}}
