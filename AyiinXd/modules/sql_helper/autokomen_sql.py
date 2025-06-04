4e824dd66ccafbe4950641306255f49332004b0a
ixall98 <sunart15up@gmail.com>
2025-06-04 22:49:27 +0700

    Update autokomen_sql.py

--- AyiinXd/modules/sql_helper/autokomen_sql.py
+++ AyiinXd/modules/sql_helper/autokomen_sql.py
@@ -16,6 +16,9 @@ class AutoKomen(BASE):
 
 BASE.metadata.create_all(bind=SESSION.get_bind())
 
+def get_all_autokomen():
+    return SESSION.query(AutoKomen).all()
+    
 def add_komen(channel_id, trigger, _):
     entry = SESSION.query(AutoKomen).filter_by(channel_id=channel_id, trigger=trigger).first()
     if not entry:
