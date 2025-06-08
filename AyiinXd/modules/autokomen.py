fa468734ee124884407bed1e71c292d09c2b8171
ixall98 <sunart15up@gmail.com>
2025-06-08 10:10:44 +0700

    Update autokomen.py

--- AyiinXd/modules/autokomen.py
+++ AyiinXd/modules/autokomen.py
@@ -9,6 +9,7 @@ import time
 from telethon.errors import FloodWaitError
 
 LAST_CHANNEL = {}  # Simpan channel terakhir per userbot sessio
+last_cheked = {}
 
 @bot.on(events.NewMessage(incoming=True))
 async def komen_comment_section(event):
