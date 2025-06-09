512cd48098b30e616837da0444fb4ec41d0b6e4f
ixall98 <sunart15up@gmail.com>
2025-06-09 10:25:01 +0700

    Create autoban_sql.py

---
+++ AyiinXd/modules/sql_helper/autoban_sql.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, String, Boolean
+from modules.sql_helper import BASE, SESSION
+
+class AutoBanChannel(BASE):
+    __tablename__ = "autoban_channel"
+    channel_id = Column(String, primary_key=True)
+    channel_username = Column(String)
+    enabled = Column(Boolean, default=True)
+
+    def __init__(self, channel_id, channel_username):
+        self.channel_id = channel_id
+        self.channel_username = channel_username
+        self.enabled = True
+
+BASE.metadata.create_all()
+
+def add_channel(channel_id, username):
+    if not SESSION.get(AutoBanChannel, channel_id):
+        ch = AutoBanChannel(channel_id, username)
+        SESSION.add(ch)
+        SESSION.commit()
+
+def remove_channel(channel_id):
+    ch = SESSION.get(AutoBanChannel, channel_id)
+    if ch:
+        SESSION.delete(ch)
+        SESSION.commit()
+
+def get_all_channels():
+    return SESSION.query(AutoBanChannel).filter_by(enabled=True).all()
