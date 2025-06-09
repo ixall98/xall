c25d96130503418e68cf4ba163bf8a2ef3eb88e8
ixall98 <sunart15up@gmail.com>
2025-06-09 10:41:09 +0700

    Update filterchannel_sql.py

--- AyiinXd/modules/sql_helper/filterchannel_sql.py
+++ AyiinXd/modules/sql_helper/filterchannel_sql.py
@@ -1,72 +1,94 @@
-from sqlalchemy import Column, String, Integer, PickleType
-from AyiinXd.modules.sql_helper import BASE, SESSION
+from sqlalchemy import Column, String, BigInteger, PickleType
+from AyiinXd import BASE, SESSION
 
-# Model table
 class FilterChannel(BASE):
     __tablename__ = "filter_channel"
-    id = Column(Integer, primary_key=True)  # auto increment
-    channel_id = Column(Integer, unique=True)  # integer ID channel
-    username = Column(String)  # username channel (optional)
-    title = Column(String)     # judul channel
-    filters = Column(PickleType)  # daftar kata filter
+    id = Column(BigInteger, primary_key=True)
+    channel_id = Column(BigInteger, unique=True)
+    username = Column(String)
+    title = Column(String)
+    filters = Column(PickleType)
+
+    def __init__(self, channel_id, username, title, filters=None):
+        self.channel_id = channel_id
+        self.username = username
+        self.title = title
+        self.filters = filters or []
 
 class LogGroup(BASE):
     __tablename__ = "log_group"
-    id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer)
+    id = Column(BigInteger, primary_key=True, default=999)
+    chat_id = Column(BigInteger)
 
 BASE.metadata.create_all(SESSION.bind)
 
-# Add channel ke DB
-def add_channel(channel_id, username=None, title=None):
-    existing = SESSION.query(FilterChannel).filter_by(channel_id=channel_id).first()
-    if not existing:
-        new = FilterChannel(channel_id=channel_id, username=username, title=title, filters=[])
-        SESSION.add(new)
+# Fungsi
+def add_channel(username):
+    from telethon.tl.functions.channels import GetFullChannelRequest
+    from AyiinXd import bot
+    import asyncio
+
+    async def get_entity_info():
+        entity = await bot.get_entity(username)
+        return entity.id, entity.username, entity.title
+
+    loop = asyncio.get_event_loop()
+    channel_id, uname, title = loop.run_until_complete(get_entity_info())
+
+    data = FilterChannel(channel_id=channel_id, username=uname, title=title)
+    SESSION.add(data)
+    SESSION.commit()
+
+def delete_channel(username):
+    data = SESSION.query(FilterChannel).filter_by(username=username).first()
+    if data:
+        SESSION.delete(data)
         SESSION.commit()
 
-def delete_channel(channel_id):
-    row = SESSION.query(FilterChannel).filter_by(channel_id=channel_id).first()
-    if row:
-        SESSION.delete(row)
-        SESSION.commit()
-
-def get_channel(channel_id):
-    return SESSION.query(FilterChannel).filter_by(channel_id=channel_id).first()
+def get_channel(username):
+    return SESSION.query(FilterChannel).filter_by(username=username).first()
 
 def get_all():
     return SESSION.query(FilterChannel).all()
 
-def add_filter(channel_id, word):
-    row = get_channel(channel_id)
-    if row:
-        if row.filters is None:
-            row.filters = []
-        if word not in row.filters:
-            row.filters.append(word)
+def add_filter(username, word):
+    data = get_channel(username)
+    if data:
+        filters = data.filters or []
+        if word not in filters:
+            filters.append(word)
+            data.filters = filters
             SESSION.commit()
 
-def remove_filter(channel_id, word):
-    row = get_channel(channel_id)
-    if row and row.filters and word in row.filters:
-        row.filters.remove(word)
-        SESSION.commit()
+def remove_filter(username, word):
+    data = get_channel(username)
+    if data and data.filters:
+        filters = data.filters
+        if word in filters:
+            filters.remove(word)
+            data.filters = filters
+            SESSION.commit()
 
-# Last used channel (biar bisa dipakai .addfilter/delfilter tanpa harus sebutin channel)
-_last_channel = {}
-
-def set_last(channel_id):
-    _last_channel["last"] = channel_id
+def set_last(username):
+    with open("last_filter_channel.txt", "w") as f:
+        f.write(username)
 
 def get_last():
-    return _last_channel.get("last")
+    try:
+        with open("last_filter_channel.txt", "r") as f:
+            return f.read().strip()
+    except:
+        return None
 
-# Set/Get grup log
 def set_log_group(chat_id):
-    SESSION.query(LogGroup).delete()  # cuma 1 log group
-    SESSION.add(LogGroup(id=999, chat_id=chat_id))
+    data = SESSION.query(LogGroup).first()
+    if not data:
+        data = LogGroup(chat_id=chat_id)
+        SESSION.add(data)
+    else:
+        data.chat_id = chat_id
     SESSION.commit()
 
 def get_log_group():
-    row = SESSION.query(LogGroup).first()
-    return row.chat_id if row else None
+    data = SESSION.query(LogGroup).first()
+    return data.chat_id if data else None
